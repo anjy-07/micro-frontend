8065222676a2b1996a73c67f7f94fcbb8b4bfef2
Anjali Verma <anjaliverma471@gmail.com>
2020-05-28 21:19:26 +0530

    Sample working Code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import Graphs from './Graphs'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">  
+//         <Graphs username="wesbos"></Graphs>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+import * as React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import Graphs from './Graphs'
+
+class ReactElement extends HTMLElement {
+  username = '';
+  constructor() {
+    super();
+    this.observer = new MutationObserver(() => this.update());
+    this.observer.observe(this, { attributes: true });
+  }
+
+  connectedCallback() {
+    this._innerHTML = this.innerHTML;
+    this.username = this.getAttribute("username");
+    console.log("username", this.username)
+    this.mount();
+  }
+
+  disconnectedCallback() {
+    this.unmount();
+    this.observer.disconnect();
+  }
+
+  update() {
+    this.unmount();
+    this.mount();
+  }
+
+  mount() {
+    const propTypes = Graphs.propTypes ? Graphs.propTypes : {};
+    console.log(this.attributes)
+    console.log(propTypes)
+    const props = {
+      ...this.getProps(this.attributes, propTypes),
+    };
+    console.log(props)
+    render(<Graphs {...props} />, this);
+  }
+
+  unmount() {
+    unmountComponentAtNode(this);
+  }
+
+  getProps(attributes, propTypes) {
+    propTypes = propTypes|| {};
+    let arr  =  [ ...attributes ]         
+      .filter(attr => attr.name !== 'style')         
+      .map(attr => this.convert(propTypes, attr.name, attr.value))
+      .reduce((props, prop) => 
+        ({ ...props, [prop.name]: prop.value }), {});
+    return arr 
+  }
+
+  convert(propTypes, attrName, attrValue) {
+    const propName = Object.keys(propTypes)
+      .find(key => key.toLowerCase() == attrName);
+    let value = attrValue;
+    if (attrValue === 'true' || attrValue === 'false') 
+      value = attrValue == 'true';      
+    else if (!isNaN(attrValue) && attrValue !== '') 
+      value = +attrValue;      
+    else if (/^{.*}/.exec(attrValue)) 
+      value = JSON.parse(attrValue);
+    return {         
+      name: propName ? propName : attrName,         
+      value: value      
+    };
+  }
 }
 
-export default App;
+customElements.define('react-el', ReactElement);
